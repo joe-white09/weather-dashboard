afdcb832e414d9f102aa423ca3be442f2c48e243
joe-white09 <joe_white09@yahoo.com>
2022-05-25 22:49:48 -0500

    get forecast now displays current conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
 var cityInputEl = document.querySelector("#city-name");
 var cityFormEl = document.querySelector("#city-input");
 var searchedCitiesListEl = document.querySelector("#city-list");
-// var searchedCities = document.querySelector("#city-list");
+// var currentWeatherEl = document.querySelector("#current-weather");
+var forecastEl = document.querySelector("#forecast");
+var currentDate = moment().format('MM/D/YYYY');
+var currentCityEl =document.querySelector("#current-city");
+var currentTempEl = document.querySelector("#current-temp");
+var currentWindEl = document.querySelector("#current-wind");
+var currentHumidityEl = document.querySelector("#current-humidity");
 
 
 // api call to get the forecast
 function getForecast(cityName) {
     var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=db90f37516c305d620d0e44557fd5bf2";
     var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=6123d81d4878931a77a13018b22cb0ab";
-   
+//    call for the current weather
     fetch(currentWeatherUrl).then(function(response) {
     
         if (response.ok) {
-         response.json().then(function(data) {
+         return response.json().then(function(data) {
             console.log(data);
-            // for(var i = 0; i<data.list.length; i++) {
-            //     console.log(data.list[i].weather);
-            //     console.log(data.list[i].main.temp);
-            // }
+            currentCityEl.innerHTML= cityName + " " + currentDate; 
+            currentTempEl.innerHTML = "Temperature: " + data.main.temp + "\xB0" +"F";
+            currentWindEl.innerHTML = "Wind: " + data.wind.speed + " mph";
+            currentHumidityEl.innerHTML = "Humidity: " + data.main.humidity + "%";
+
         });
         } else {
         alert("City not found");
@@ -26,14 +33,14 @@ function getForecast(cityName) {
 .catch(function(error) {
     alert("Unable to connect to OpenWeather");
 });
+// call for the forecast
     fetch(forecastUrl).then(function(response) {
     
         if (response.ok) {
          response.json().then(function(data) {
-            console.log(data);
             for(var i = 0; i<data.list.length; i+=8) {
                 console.log(data.list[i]);
-                // console.log(data.list[i].main.temp);
+                
             }
         });
         } else {
@@ -45,6 +52,7 @@ function getForecast(cityName) {
 });
 };
 
+// function to call upon name on the buttons
 function recallForecast(event) {
     var cityName = event.target.getAttribute("city");
     getForecast(cityName);
@@ -69,7 +77,7 @@ var formSubmitHandler = function(event) {
 };
 
 
-// getApi();
 
+// event listeners
 cityFormEl.addEventListener("submit", formSubmitHandler);
 searchedCitiesListEl.addEventListener("click", recallForecast);
