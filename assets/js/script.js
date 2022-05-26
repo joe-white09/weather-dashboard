53ac198002a980066e956583e262a9bea345b9ed
joe-white09 <joe_white09@yahoo.com>
2022-05-25 19:01:48 -0500

    updated api calls to fetch current and target forecast days

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,18 +6,43 @@ var searchedCitiesListEl = document.querySelector("#city-list");
 
 // api call to get the forecast
 function getForecast(cityName) {
-    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=5&appid=db90f37516c305d620d0e44557fd5bf2";
-
-    fetch(requestUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(data) {
-        console.log(data);
-
-    });
-
-
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=db90f37516c305d620d0e44557fd5bf2";
+    var currentWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=6123d81d4878931a77a13018b22cb0ab";
+   
+    fetch(currentWeatherUrl).then(function(response) {
+    
+        if (response.ok) {
+         response.json().then(function(data) {
+            console.log(data);
+            // for(var i = 0; i<data.list.length; i++) {
+            //     console.log(data.list[i].weather);
+            //     console.log(data.list[i].main.temp);
+            // }
+        });
+        } else {
+        alert("City not found");
+        }
+})
+.catch(function(error) {
+    alert("Unable to connect to OpenWeather");
+});
+    fetch(forecastUrl).then(function(response) {
+    
+        if (response.ok) {
+         response.json().then(function(data) {
+            console.log(data);
+            for(var i = 0; i<data.list.length; i+=8) {
+                console.log(data.list[i]);
+                // console.log(data.list[i].main.temp);
+            }
+        });
+        } else {
+        alert("City not found");
+        }
+})
+.catch(function(error) {
+    alert("Unable to connect to OpenWeather");
+});
 };
 
 function recallForecast(event) {
@@ -33,13 +58,14 @@ var formSubmitHandler = function(event) {
     if (cityName) {
         getForecast(cityName);
         cityInputEl.value = "";
+        var searchedCities = document.createElement("button");
+        searchedCities.innerHTML = cityName;
+        searchedCities.setAttribute("city", cityName);
+        searchedCitiesListEl.appendChild(searchedCities);
     } else {
         alert("Please enter a valid city name")
     };
-    var searchedCities = document.createElement("button");
-    searchedCities.innerHTML = cityName;
-    searchedCities.setAttribute("city", cityName);
-    searchedCitiesListEl.appendChild(searchedCities);
+
 };
 
 
